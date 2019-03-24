2b200dddcca536d08881048d7a8f6ef91a89ff2c
xuezhisd <xuezhi@126.com>
2019-03-24 18:35:21 +0800

    compile successfully in ubuntu16.04 & ros kinetic

--- lsd_slam_core/src/util/settings.cpp
+++ lsd_slam_core/src/util/settings.cpp
@@ -35,7 +35,8 @@ bool autoRunWithinFrame = true;
 int debugDisplay = 0;
 
 bool onSceenInfoDisplay = true;
-bool displayDepthMap = true;
+//bool displayDepthMap = true;
+bool displayDepthMap = false;
 bool dumpMap = false;
 bool doFullReConstraintTrack = false;
 
